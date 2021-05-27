edaf9d26b5d8d967de86689c88b14ebdc8efd0b1
zeroxerxes <64868373+north-shore@users.noreply.github.com>
2021-05-27 01:37:43 +0100

    Initial commit

---
+++ footer.php
@@ -0,0 +1,86 @@
+<footer class="page-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-6 col-md-3">
+                    <h4>Help</h4>
+                    <ul class="footer-links">
+                        <li><a href="owlinfo/shipping.html">Shipping</a></li>
+                        <li><a href="owlinfo/returns.html">Returns</a></li>
+                        <li><a href="contact.html">contact</a></li>
+                    </ul>
+                </div>
+                <div class="col-6 col-md-3">
+                    <h4>Account</h4>
+                    <ul class="footer-links">
+                        <li><a href="login.php">Login</a></li>
+                        <li><a href="owlinfo/order.html">Order Status</a></li>
+                    </ul>
+                </div>
+                <div class="col-6 col-md-3">
+                    <h4>Come to us </h4>
+                    <ul class="footer-links">
+                        <li><a href="contact.html">Contact</a></li>
+                        <li><a href="contact.html">Email</a></li>
+                    </ul>
+                    <h4>Doing Business Together </h4>
+                    <ul class="footer-links">
+                        <li><a href="owlinfo/buying.html">Buying Guns Online</a></li>
+                        <li><a href="user-registration.php">Sign up to receive Promotion Coupon</a></li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <footer id="footerpad">
+                        <div class="container">
+                            <div class="row" style="margin-top: -4em;">
+                                <div class="col-md-6 col-lg-8 mx-auto">
+                                    <ul class="list-inline text-center">
+                                        
+                                        <li class="list-inline-item"><a href="https://web.facebook.com/Owlsteel/"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-facebook fa-stack-1x fa-inverse"></i></span></a></li>
+                                    </ul><p class="copyright text-muted text-center">Copyright &copy; Your Company 2020 |</p></div>
+                            </div>
+                        </div>
+                    </footer>
+                </div>
+                <h1 class="logo" style="margin-top:30px;"></h1>
+            </div>
+            <p class="shake animated">SHOP GUNS WITH NI SERIAL NUMBER&nbsp; &nbsp;THIS PAGE&nbsp; IS PASSKEY PROTECTED YOU WOULD HAVE TO REQUEST PASS&nbsp; &nbsp;&nbsp;<a href="requestPK.html" style="color: red;">HERE</a></p>
+            <hr>
+            <div class="footer-legal">
+                <div class="float-md-right region"><a href="#"><img src="assets/img/united-states-flag.png">United States</a></div>
+                <div class="d-inline-block copyright">
+                    <p class="d-inline-block">Copyright © 2020. All rights reserved.<br></p>
+                </div>
+                <div class="d-inline-block legal-links">
+                    <div class="d-inline-block item">
+                        <h5>Privacy Policy</h5>
+                    </div>
+                    <div class="d-inline-block item">
+                        <h5>Terms of Use</h5>
+                    </div>
+                    <div class="d-inline-block item">
+                        <h5>Legal</h5>
+                    </div>
+                    <div class="d-inline-block item">
+                        <h5>License</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/bs-init.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.2.0/aos.js"></script>
+    <script src="assets/js/--mp---Multiple-items-slider-responsive-1.js"></script>
+    <script src="assets/js/--mp---Multiple-items-slider-responsive.js"></script>
+    <script src="assets/js/current-day.js"></script>
+    <script src="assets/js/e-commerce-slideshow.js"></script>
+    <script src="assets/js/featured-products-slider.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.1/js/swiper.jquery.min.js"></script>
+    <script src="assets/js/Multi-step-form.js"></script>
+    <script src="assets/js/MUSA_product-display.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+</body>
+
+</html
---
+++ header.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Gun store OWL AMMO AMMO Order guns online for good prices ">
+    <meta name="keywords" content="gunstore ,owlsteelammo ,ammo owlsteel , guns ,gunshop, ammo,shop,">
+    <meta name="Zero" content="gun store ">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Owl Steel</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:400,600,800">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Quicksand">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/--mp---Multiple-items-slider-responsive.css">
+    <link rel="stylesheet" href="assets/css/Animated-CSS-Waves-Background-SVG.css">
+    <link rel="stylesheet" href="assets/css/Bold-BS4-Footer-Big-Logo.css">
+    <link rel="stylesheet" href="assets/css/Bootstrap-4---Photo-Gallery.css">
+    <link rel="stylesheet" href="assets/css/Carousel-Hero.css">
+    <link rel="stylesheet" href="assets/css/Contact-Form-Clean.css">
+    <link rel="stylesheet" href="assets/css/Corporate-Footer-Clean-1.css">
+    <link rel="stylesheet" href="assets/css/Corporate-Footer-Clean.css">
+    <link rel="stylesheet" href="assets/css/DA_About.css">
+    <link rel="stylesheet" href="assets/css/Drag--Drop-Upload-Form.css">
+    <link rel="stylesheet" href="assets/css/e-commerce-slideshow-1.css">
+    <link rel="stylesheet" href="assets/css/e-commerce-slideshow.css">
+    <link rel="stylesheet" href="assets/css/featured-products-slider-1.css">
+    <link rel="stylesheet" href="assets/css/featured-products-slider.css">
+    <link rel="stylesheet" href="assets/css/Footer-with-social-media-icons.css">
+    <link rel="stylesheet" href="assets/css/gradient-navbar-1.css">
+    <link rel="stylesheet" href="assets/css/gradient-navbar.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.2.0/aos.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.1/css/swiper.min.css">
+    <link rel="stylesheet" href="assets/css/Multi-step-form.css">
+    <link rel="stylesheet" href="assets/css/MUSA_carousel-product-cart-slider-1.css">
+    <link rel="stylesheet" href="assets/css/MUSA_carousel-product-cart-slider.css">
+    <link rel="stylesheet" href="assets/css/MUSA_product-display-1.css">
+    <link rel="stylesheet" href="assets/css/MUSA_product-display.css">
+    <link rel="stylesheet" href="assets/css/Newsletter-Subscription-Form.css">
+    <link rel="stylesheet" href="assets/css/OcOrato---Login-form-1.css">
+    <link rel="stylesheet" href="assets/css/OcOrato---Login-form.css">
+    <link rel="stylesheet" href="assets/css/Registration-Form-with-Photo.css">
+    <link rel="stylesheet" href="assets/css/Responsive-Card-Item-List.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+	
+	<link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+	<link rel="manifest" href="/manifest.json">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+	<meta name="theme-color" content="#ffffff">
+    <!--import bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+
+
+</head>
+ <!-- Messenger Chat Plugin Code -->
+    <div id="fb-root"></div>
+
+    <!-- Your Chat Plugin code -->
+    <div id="fb-customer-chat" class="fb-customerchat">
+    </div>
+
+    <script>
+      var chatbox = document.getElementById('fb-customer-chat');
+      chatbox.setAttribute("page_id", "107684898173578");
+      chatbox.setAttribute("attribution", "page_inbox");
+      window.fbAsyncInit = function() {
+        FB.init({
+          xfbml            : true,
+          version          : 'v10.0'
+        });
+      };
+
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+    </script>
+<body style="/*background: linear-gradient(rgba(0, 23, 15, 0.65), rgba(47, 23, 15, 0.65)), url(&quot;assets/img/Screenshot.png&quot;);*/">  
+<nav class="navbar navbar-light navbar-expand-lg bg-dark py-lg-4" id="mainNav">
+    <div class="container"><a class="navbar-brand text-uppercase d-lg-none text-expanded" href="index.php">OWLSTEEL</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav  "  >
+                    <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="about.html">About us</a></li>
+                    
+                    <li class="nav-item"><a class="nav-link" href="product/bestsale.html">Store</a></li>
+                    <li class="nav-item"><a class="nav-link" href="contact.html">contact</a></li>
+    
+    
+                </ul><a href="request.html" style="font-size: 16px;color: red;">Request ammoniutiion</a>
+                <ul class="nav navbar-nav">
+                    <li class="nav-item"></li>
+                    <li class="nav-item"></li>
+                    <li class="nav-item"></li>
+                </ul><a href="login.php" style="color: white;">Login</a><a href="#">/</a><a href="user-registration.php" style="color: white;">Register</a></div>
+        </div>
+    </nav>
+<body style="/*background: linear-gradient(rgba(0, 23, 15, 0.65), rgba(47, 23, 15, 0.65)), url(&quot;assets/img/Screenshot.png&quot;);*/">  
